b5cfa7dde03d8549d3a27e24435fbb3824afb3a4
ahmedadelelshaer <138902289+ahmedadelelshaer@users.noreply.github.com>
2024-12-23 23:07:46 +0200

    Update Order_controller.js

--- Back/controller/Order_controller.js
+++ Back/controller/Order_controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Order = require('../models/Order');
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 // Middleware to verify JWT and extract user information
